32cb1f8c7379e830239b99a69ab5b59277d885e9
DiegoMiguelM <diegomiranda2007@cesanrafael.org>
2024-11-19 14:20:45 -0400

    proyevtojavita

---
+++ JavaScript/js main.js
@@ -0,0 +1,43 @@
+let mensaje="Hola Mundo";
+console.log(mensaje);
+
+let numerouno=2;
+let numerodos=4;
+let numerotres=10;
+let resultadoSuma=numerouno+numerodos+numerotres;
+console.log(resultadoSuma);
+console.log("La suma es ",resultadoSuma);
+let resultadoResta=numerouno-numerodos-numerotres
+console.log(resultadoResta);
+console.log("La resta es ",resultadoResta);
+let resultadoDivision=numerouno/numerodos/numerotres
+console.log(resultadoDivision);
+console.log("La division es ",resultadoDivision);
+function Sumar(){
+let numerouno=3
+let numerodos=4
+let numerotres=10
+let resultado=numerouno+numerodos+numerotres;
+alert("La suma es "+resultado);
+}
+function Restar(){
+    let numerouno=3
+    let numerodos=4
+    let numerotres=10
+    let resultado=numerouno-numerodos-numerotres;
+    alert("La resta es "+resultado);
+    }
+    function Dividir(){
+        let numerouno=3
+        let numerodos=4
+        let numerotres=10
+        let resultado=numerouno/numerodos/numerotres;
+        alert("La division es "+resultado);
+        }
+        function Multiplicar(){
+            let numerouno=3
+            let numerodos=4
+            let numerotres=10
+            let resultado=numerouno*numerodos*numerotres;
+            alert("La multiplicasion es "+resultado);
+            }
